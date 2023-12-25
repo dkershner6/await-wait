cb0b31e8f55f9732d4470c91e09fb2105fe280e4
Derek Kershner <dkershner@dkershner.com>
2023-12-25 15:39:08 -0800

    Release

--- .projenrc.ts
+++ .projenrc.ts
@@ -13,10 +13,16 @@ const project = new typescript.TypeScriptProject({
 
     // deps: [],                /* Runtime dependencies of this module. */
     // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-    devDeps: ["dkershner6-projen"] /* Build dependencies for this module. */,
+    devDeps: [
+        "dkershner6-projen",
+        "projen-nvm",
+    ] /* Build dependencies for this module. */,
     // packageName: undefined,  /* The "name" in package.json. */
 
     ...BASE_PROJECT_OPTIONS_NODE_20,
+    release: true,
+    releaseToNpm: true,
+    github: true,
 });
 
 enactBaseProjectConfig(project);
