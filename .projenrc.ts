68b21276cbc8e5a166fc66c032fc3bf7b9b931b1
Derek Kershner <dkershner@dkershner.com>
2023-12-29 14:16:55 -0800

    feat: package now has description and keywords (again)

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,11 +2,21 @@ import { Node20TypeScriptProject } from "dkershner6-projen";
 import { TextFile } from "projen";
 import { Nvmrc } from "projen-nvm";
 
+const PACKAGE_NAME = "await-wait";
+
 const project = new Node20TypeScriptProject({
     majorVersion: 1,
 
     defaultReleaseBranch: "main",
-    name: "await-wait",
+    name: PACKAGE_NAME,
+    keywords: ["wait", "promise", "await"],
+    description:
+        "Very simple wait function for async Javascript/TypeScript Promises. Surprisingly, nothing similar existed at time of writing.",
+    homepage: `https://github.com/dkershner6/${PACKAGE_NAME}#readme`,
+    bugsUrl: `https://github.com/dkershner6/${PACKAGE_NAME}/issues`,
+    authorName: "Derek Kershner",
+    authorUrl: "https://dkershner.com",
+    repository: `git+https://github.com/dkershner6/${PACKAGE_NAME}.git`,
     projenrcTs: true,
 
     // deps: [],                /* Runtime dependencies of this module. */
@@ -20,6 +30,8 @@ const project = new Node20TypeScriptProject({
     release: true,
     releaseToNpm: true,
     github: true,
+
+    docgen: true,
 });
 
 new Nvmrc(project);
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# replace this
+# await-wait
+
+Easily asynchronously wait for a specified time.
+
+```typescript
+import { wait } from 'await-wait';
+
+await wait(1000);
+// One second has passed
+await doAction();
+```
